71bdb8158bea95c1d77c312a5c0e525b5f331aa7
Gil Moura <gilmoura82@gmail.com>
2022-10-19 10:14:30 -0300

    Calculadora em Java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         int resultado = 0;
-        String msg, oper;
+        String msg, oper = null;
         String a = "";
         String b = "";
 
